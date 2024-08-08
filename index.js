960f7d373fad3c219bb50fdb269a136db89840e7
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:27:32 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Get started!
+
+let name = prompt("Create a username")
+let num= prompt("How old are you?")
+let age = parseInt(num)
+console.log("You are age is eligible to play" + age)
+console.log("Welcome to my game, "+ name)
+
+let durkFan= prompt("What's your favorite lil durk song?")
+
+let durkSong= ("Backdoor","Play with us","India")
+console.log("Currently playing, "+ durkFan)
+
